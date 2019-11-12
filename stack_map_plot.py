aafd0b04b3bef3c5d15736f89a074156ca154c07
John <jfr10@le.ac.uk>
2019-11-12 22:53:14 +0000

    Plotly: it's good.

--- stack_map_plot.py
+++ stack_map_plot.py
@@ -1,8 +1,7 @@
 import gdal
 from pathlib import Path
-import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
+import plotly.graph_objects as go
 
 import pdb
 
@@ -13,17 +12,18 @@ def test_stack_map_plot():
 
 
 def stack_map_plot(raster_path_list, label_spec = None, colormap_spec=None):
-    fig = plt.figure()
-    axis = fig.gca(projection='3d')
-    for ii, raster_path in enumerate(raster_path_list[0:2]):
+    Z = []
+    for ii, raster_path in enumerate(raster_path_list):
         band = gdal.Open(raster_path)
         band_array = band.ReadAsArray()
         nodata = band_array.min()
-        band_array[band_array == nodata] = np.median(band_array)
+        band_array[band_array == nodata] = np.nan
         gt = band.GetGeoTransform()
         X = np.arange(gt[0], gt[0] + gt[1]*band.RasterXSize, gt[1])
         Y = np.arange(gt[3], gt[3] + gt[5]*band.RasterYSize*-1, gt[5]*-1)   # Geotransforms.
         X, Y = np.meshgrid(X, Y)
-        Z = band_array * ii
-        axis.plot_surface(X, Y, Z)
-    plt.show()   
+        Z.append(band_array + (ii*10))
+
+    data = [go.Surface(z=z_map) for z_map in Z]
+    fig = go.Figure(data=data)
+    fig.show()
