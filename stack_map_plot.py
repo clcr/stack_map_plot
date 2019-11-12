7ca547f675125320e872f59c4cfa4e359bb37f84
John <jfr10@le.ac.uk>
2019-11-12 22:38:15 +0000

    mpl...sortof getting there, trying plotly

--- stack_map_plot.py
+++ stack_map_plot.py
@@ -1,17 +1,29 @@
 import gdal
+from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
+import pdb
+
 def test_stack_map_plot():
-    in_raster = "some_raster.tif"
-    stack_map_plot("some_raster.tif", labels)
+    data_dir = Path("./data")
+    raster_list = [str(x) for x in data_dir.glob("*.tif")]
+    stack_map_plot(raster_list)
 
 
-def stack_map_plot(in_raster_path, label_spec, colormap_spec=None):
-    raster_array  = gdal.Open(in_raster_path).ReadAsArray()
-    axis = plt.figure().gca(projection="3d")
-    for band in raster_array[:,...]:
-        axis.add(band)
-        axis.setcolormap(colormp)
-        axis.setlabel(label)
+def stack_map_plot(raster_path_list, label_spec = None, colormap_spec=None):
+    fig = plt.figure()
+    axis = fig.gca(projection='3d')
+    for ii, raster_path in enumerate(raster_path_list[0:2]):
+        band = gdal.Open(raster_path)
+        band_array = band.ReadAsArray()
+        nodata = band_array.min()
+        band_array[band_array == nodata] = np.median(band_array)
+        gt = band.GetGeoTransform()
+        X = np.arange(gt[0], gt[0] + gt[1]*band.RasterXSize, gt[1])
+        Y = np.arange(gt[3], gt[3] + gt[5]*band.RasterYSize*-1, gt[5]*-1)   # Geotransforms.
+        X, Y = np.meshgrid(X, Y)
+        Z = band_array * ii
+        axis.plot_surface(X, Y, Z)
+    plt.show()   
