06c9e62eb22483f7136744c98eaa6381dcd8e5bd
jfr10 <jfr10@le.ac.uk>
2019-11-13 14:49:46 +0000

    Labels and tick labels added

--- stack_map_plot.py
+++ stack_map_plot.py
@@ -1,4 +1,5 @@
 import gdal
+import os
 from pathlib import Path
 import numpy as np
 import plotly.graph_objects as go
@@ -13,19 +14,46 @@ def test_stack_map_plot():
 
 def stack_map_plot(raster_path_list, label_spec = None, colormap_spec=None):
     data = []
+    zlabel = []
+    zpos = []
     for ii, raster_path in enumerate(raster_path_list):
+        # Get and filter raster values
         band = gdal.Open(raster_path)
         band_array = band.ReadAsArray()
         nodata = band_array.min()
         band_array[band_array == nodata] = np.nan
+        
+        # Calulate x and y axis from geotransforms: top-left corner and stride from GT, number
+        # of strides from RasterX/Ysize
         gt = band.GetGeoTransform()
         X = np.arange(gt[0], gt[0] + gt[1]*band.RasterXSize, gt[1])
-        Y = np.arange(gt[3], gt[3] + gt[5]*band.RasterYSize*-1, gt[5]*-1)   # Geotransforms.
+        Y = np.arange(gt[3], gt[3] + gt[5]*band.RasterYSize*-1, gt[5]*-1)   # Geotransforms are a pain..
+        
+        # Set Z to offsets and build colormap
         Z = np.ones(band_array.shape)
+        Z = Z+(ii*10)
         Z[band_array == nodata] = np.nan
-        Z = Z+(ii*5)
-        data.append(go.Surface(x=X, y= Y, z=Z, surfacecolor=band_array))
+        nan_colorscale = [[0,'grey'],
+                          [0.01, 'rgba(0,0,0,1)'],
+                          [0.01, 'darkgoldenrod'],
+                          [1, 'mistyrose']]   
+        data.append(go.Surface(x=X, y= Y, z=Z, surfacecolor=band_array, connectgaps = False,
+            colorscale=nan_colorscale, cmin = band_array.min(), cmax = band_array.max()))
+        
+        # Setting labels and positions
+        zlabel.append(os.path.basename(raster_path))
+        zpos.append(1+(ii*10))
         if ii > 0:
             data[ii].showscale=False
+    
+    #Creating figure and setting 
     fig = go.Figure(data=data)
+    fig.update_layout(scene = {
+        "zaxis": {
+            "ticktext":zlabel,
+            "tickvals":zpos
+            },
+        "xaxis_title": "lon",
+        "yaxis_title": "lat"
+        })
     fig.show()
